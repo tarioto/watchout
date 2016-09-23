bac77ade03df68d2d18b20b34d54d9ddb64ba6d9
tarioto <timarioto@gmail.com>
2016-09-23 16:53:20 -0700

    Add board svg with d3

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,11 @@
 // start slingin' some d3 here.
+var width = 800;
+var height = 640;
+
+var svg = d3.select('body').append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+svg.append('rect').attr('width', width)
+  .attr('height', height).attr('stroke', 'blue')
+  .attr('stroke-width', '5px');
